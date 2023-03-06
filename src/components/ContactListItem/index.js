5405131f37e23e1c63213507e01a2a85cffd5644
samuelfr98 <46605132+samuelfr98@users.noreply.github.com>
2023-03-05 19:16:42 -0600

    Can create new chat room in backend from contacts page

--- src/components/ContactListItem/index.js
+++ src/components/ContactListItem/index.js
@@ -3,25 +3,63 @@ import { useNavigation } from "@react-navigation/native";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { API, Auth, graphqlOperation } from "aws-amplify";
+import { createChatRoom, createUserChatRoom } from "../../graphql/mutations";
 
 dayjs.extend(relativeTime);
 
 const ContactListItem = ({
   user,
-  onPress = () => {},
+  WhutonPress = () => {},
   selectable = false,
   isSelected = false,
 }) => {
   const navigation = useNavigation();
 
+  const onPress = async () => {
+    console.warn("Pressed");
+
+    // Check if we already have a ChatRoom with user
+
+    // Create a new ChatRoom
+    const newChatRoomData = await API.graphql(
+      graphqlOperation(createChatRoom, {
+        input: {},
+      })
+    );
+    console.log(newChatRoomData);
+
+    if (!newChatRoomData.data?.createChatRoom) {
+      console.log("Error creating the chat room");
+    }
+    const newChatRoom = newChatRoomData.data?.createChatRoom;
+    // Add the clicked user to the ChatRoom
+    await API.graphql(
+      graphqlOperation(createUserChatRoom, {
+        input: {
+          chatRoomId: newChatRoom.id,
+          userId: user.id,
+        },
+      })
+    );
+
+    // Add the auth user to the ChatRoom
+    const authUser = await Auth.currentAuthenticatedUser();
+    await API.graphql(
+      graphqlOperation(createUserChatRoom, {
+        input: {
+          chatRoomId: newChatRoom.id,
+          userId: authUser.attributes.sub,
+        },
+      })
+    );
+
+    // Navigate to the newly created ChatRoom
+    navigation.navigate("Chat", { id: newChatRoom.id });
+  };
+
   return (
-    <Pressable
-      style={styles.container}
-      onPress={() => {
-        // navigation.navigate("Contacts", { id: user.id, name: user.name });
-        onPress;
-      }}
-    >
+    <Pressable style={styles.container} onPress={onPress}>
       <Image source={{ uri: user.image }} style={styles.image} />
 
       <View style={styles.content}>
@@ -55,7 +93,6 @@ const styles = StyleSheet.create({
     flex: 1,
     // borderBottomWidth: StyleSheet.hairlineWidth,
     // borderBottomColor: "lightgray",
-    
   },
   image: {
     width: 60,
